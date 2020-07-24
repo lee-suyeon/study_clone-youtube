08f684b99fceabe54f33bc3b31963fabcb698da1
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-07-24 17:03:22 +0900

    로그아웃 기능 구현

--- index.js
+++ index.js
@@ -65,8 +65,28 @@ app.post('/api/users/login', (req, res) => {
 
 /******************** Authentication  ********************/
 app.get('/api/users/auth', auth, (req, res) => {
-
+  // 여기 까지 미들웨어를 통과해 왔다는 얘기는 인증이 true일때
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.roll === 0 ? false : true, // roll이 0이면 일반 유저 0이 아니면 관리자 
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
 })
 
+
+/******************** LOGOUT  ********************/
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: ''}, (err, user) => {
+    if(err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
