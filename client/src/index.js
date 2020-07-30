c43ff7d8231f6c6bb7ac119dd5f3d458516bcc98
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-07-30 12:32:32 +0900

    Add stylesheet

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from "react-router-dom";
